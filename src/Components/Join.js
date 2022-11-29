e0ac6fa366a005657df4bf577c27dc2cb29db379
vibe008 <104353180+vibe008@users.noreply.github.com>
2022-11-29 12:19:52 +0530

    Update Join.js

--- src/Components/Join.js
+++ src/Components/Join.js
@@ -11,7 +11,7 @@ function Join() {
                 <img className='w-full h-full' src={img} alt="" />
             </div>
 
-            <div className="max-w-[320px] -ml-[30px]">
+            <div className="max-w-[320px] ml-[13px]">
                 <h2 className='text-[40px] leading-[40px] font-extrabold text-white'>wanna join <span>&</span> have fun</h2>
                 <p className= 'font-secondary font-normal text-[16px] leading-[26px] md:text-[20px] md:leading-[32px] text-white max-w-[348px] md:max-w-[470px] lg:max-w-[492px]'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit facilis praesentium incidunt eos sequi quae ad eligendi aut harum. </p>
                 <button className='flex justify-center items-center rounded-[1px] bg-white text-black hover:bg-black hover:text-white transition font-primary font-bold uppercase text-[14px] leading-[18px] h-[46px] px-[40px] tracking-[1px]'> join now</button>
